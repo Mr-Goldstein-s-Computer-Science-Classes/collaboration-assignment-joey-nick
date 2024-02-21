70178e371d90332bca301c1146064e52d60038fa
nickrigatoni <113187682+nickrigatoni@users.noreply.github.com>
2024-02-21 12:43:06 -0500

    method outlines

--- Gradebook.java
+++ Gradebook.java
@@ -1,2 +1,54 @@
+import java.util.ArrayList;
 public class Gradebook {
+
+    public Gradebook(){
+    }
+
+    public void createStudent(String name){
+
+    }
+
+    public void addAssignment(String assignment){
+
+    }
+
+    public void gradeAssignment(String assignment, String studentName, double grade){
+
+    }
+
+    public double getAverageGrade(String assignment){
+
+    }
+
+    public double getMinimumGrade(String assignment){
+
+    }
+
+    public double getMaximumGrade(String assignment){
+
+    }
+
+    public double getAverageOverall(){
+
+    }
+
+    public double getMaxOverall(){
+
+    }
+
+    public double getMinOverall(){
+
+    }
+
+    public double getStudentsGrade(){
+
+    }
+
+    public String toString(){
+
+    }
+
+    public Student getStudent(String studentName){
+
+    }
 }
